76d64097277c8116fb656fb3529333a7374f98a4
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-10-05 22:37:47 +0200

    Update main.py

--- orbis/main.py
+++ orbis/main.py
@@ -6,16 +6,17 @@ from .orbis import message, app, create_map, get_location_data_list
 
 def orbis() -> str:
     """
-    Returns the usage of Orbis.
+    Returns the usage of both orbis-cli and orbis-web.
     """
     return """
-    Orbis Unum is a geolocation & reverse-geolocation tool with two modes of operation: CLI and WEB.
+    orbis-cli/orbis-web
 
     CLI
-    ---
-       Use 'orbis-cli' command to start the command-line interface variant of Orbis. 
+    ===
+       Use 'orbis-cli' command to start the command-line interface variant of Orbis Unum. 
        In CLI mode, you should provide an IP Address or a file containing IP Addresses as an argument. 
        Orbis will fetch the geolocation information for the given IP Address(es) and display it on a map.
+
 
     Example
     -------
@@ -24,12 +25,13 @@ def orbis() -> str:
 
 
     WEB
-    ---
-       Use 'orbis-web' command to start the web variant of Orbis.
+    ===
+       Use 'orbis-web' command to start the web variant of Orbis Unum.
        In WEB mode, you will be presented with a 
        form where you can input geographical coordinates (latitude and longitude).
        After submitting the form, Orbis will fetch the geolocation information for the 
        given coordinates and display it on a map.
+
 
     Example
     -------
@@ -41,24 +43,26 @@ def orbis() -> str:
 
 def cli():
     """
-    The CLI variant of Orbis. It parses the arguments, creates the map and opens it in the default browser.
+    The CLI variant of Orbis Unum.
     """
-    if args.ip is None:
-        exit("usage: orbis-cli -h/--help")
-    # Create the directory where generated maps will be stored.
-    path_finder(["orbis-maps"])
-
-    created_map = create_map(
-        map_data=get_location_data_list(ip_address=args.ip), map_name=args.output
-    )
-    log.info(message.opening_map(created_map))
-    webbrowser.open(created_map)
+    if args.ip is not None:
+        # Create the directory where generated maps will be stored.
+        path_finder(["orbis-maps"])
+        
+        created_map = create_map(
+            map_data=get_location_data_list(ip_address=args.ip), map_name=args.output
+        )
+        
+        log.info(message.opening_map(created_map))
+        webbrowser.open(created_map)
+    else:
+        log.info("use orbis-cli -h/--help to show usage.")
 
 
 def web():
     """
-    The web variant of Orbis. It starts the Flask app.
+    The web variant of Orbis.
     """
     # Start the Flask app
     log.info("Starting Flask app at http://localhost:5000/index")
-    app.run(debug=True)
+    app.run(debug=args.debug)
