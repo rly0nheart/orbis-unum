b2bd4a9e08bf64e3cbc6c941b2ab3c01c8b4402d
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-10-30 05:14:29 +0200

    Update __init__.py

--- orbis_unum/__init__.py
+++ orbis_unum/__init__.py
@@ -1,3 +1,7 @@
 __author__ = "Richard Mwewa"
 __about__ = "https://about.me/rly0nheart"
-__version__ = "1.0.1"
+__version__ = "1.1.0"
+
+__description__ = """# Orbis Unum
+> 🌍 IP Geolocator & Coordinate Mapper 📍"""
+__epilog__ = f"# by [{__author__}]({__about__})"
